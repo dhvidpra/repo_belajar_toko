39eaeffd31e0815cacc4c7ddc00934b305ef35e1
Dhyana Vidya Pratistha <75551874+dhvidpra@users.noreply.github.com>
2022-01-11 17:12:37 +0700

    Add files via upload
    Migrasi database db_toko

---
+++ 2022_01_04_033721_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id_order');
+            $table->unsignedBigInteger('id_customer');
+            $table->date('tgl_order');
+
+            $table->foreign('id_customer')->references('id_customer')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ 2022_01_04_091424_create_detail_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_orders', function (Blueprint $table) {
+            $table->bigIncrements('id_detail_order');
+            $table->unsignedBigInteger('id_order');
+            $table->unsignedBigInteger('id_product');
+            $table->integer('qty');
+            $table->integer('subtotal');
+            
+            $table->foreign('id_order')->references('id_order')->on('orders');
+            $table->foreign('id_product')->references('id_product')->on('product');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_orders');
+    }
+}
